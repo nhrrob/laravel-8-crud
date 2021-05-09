67c08f0b0154b9d7cba419248769b5af368da3e0
Nazmul Hasan Robin <robin.sust08@gmail.com>
2021-05-09 16:56:05 +0600

    crud added

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['middleware' => 'auth'], function () { 
+  Route::resource('products', '\App\Http\Controllers\ProductController'); 
+});
